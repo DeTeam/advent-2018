e797419471022b8364ceeecc80f4506a4f9e4f9b
Timur Amirov <timur.amirov@zalando.de>
2018-12-01 11:01:45 +0100

    Implement day1 tasks

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2018
+
+This repository contains my attempts to implement AoC challenges.
+Having fun while learning some programming languages
+
+- Day1: `rust`
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,61 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn task1(s: &str) {
+    let lines = s.lines();
+    let mut result: i32 = 0;
+
+    for l in lines {
+        let sign = l.get(0..1).expect("Invalid input");
+        let value: i32 = l.get(1..).expect("Invalid input").parse().unwrap();
+
+        // println!("Sign: {}, Value: {}", sign, value);
+
+        if sign == "+" {
+            result = result + value;
+        } else {
+            result = result - value;
+        }
+    }
+    println!("First task's result: {}", result);
+}
+
+fn task2(s: &str) {
+    let lines = s.lines();
+
+    let mut results: HashSet<i32> = HashSet::new();
+    let mut result: i32 = 0;
+
+    for l in lines.cycle() {
+        let sign = l.get(0..1).expect("Invalid input");
+        let value: i32 = l.get(1..).expect("Invalid input").parse().unwrap();
+
+        // println!("Sign: {}, Value: {}", sign, value);
+
+        if sign == "+" {
+            result = result + value;
+        } else {
+            result = result - value;
+        }
+
+        if results.contains(&result) {
+            println!("Second task's result: {}", result);
+            break;
+        } else {
+            results.insert(result);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // `mut` here, because `read_to_string` takes `&mut self`
+    let mut f = File::open("input.txt").expect("File not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    task1(&contents);
+    task2(&contents);
 }
