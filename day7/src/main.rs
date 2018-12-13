57f63d15812a2085ca6ba1f31828e949739505be
Timur Amirov <timur.amirov@zalando.de>
2018-12-13 22:22:35 +0100

    Day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -25,8 +25,6 @@ fn task1(s: &str) {
             .or_insert_with(|| vec![target]);
 
         dependencies.entry(target).or_insert_with(|| vec![]);
-
-        println!("{} -> {}", source, target);
     }
 
     while dependencies.len() > 0 {
@@ -48,6 +46,82 @@ fn task1(s: &str) {
     println!("Result: {:#?}", result);
 }
 
+fn get_timing(key: &str) -> i32 {
+    let base = "A".as_bytes()[0];
+    let current = key.as_bytes()[0];
+
+    (current - base + 1) as i32 + 60
+}
+
+fn task2(s: &str) {
+    let lines = s.lines();
+
+
+    const MAX_WORKERS: usize = 5;
+    let mut dependencies: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut workers: Vec<(&str, i32)> = Vec::new();
+    let mut result = String::new();
+    let mut required_time: i32 = 0;
+
+    for line in lines {
+        let re =
+            Regex::new(r"^Step ([A-Z]) must be finished before step ([A-Z]) can begin.$").unwrap();
+        let captures = re.captures(line).expect("Failed to capture the regexp");
+
+        let source = captures.get(1).expect("Expect source option").as_str();
+        let target = captures.get(2).expect("Expect target option").as_str();
+
+        dependencies
+            .entry(source)
+            .and_modify(|e| e.push(target))
+            .or_insert_with(|| vec![target]);
+
+        dependencies.entry(target).or_insert_with(|| vec![]);
+    }
+
+    while dependencies.len() > 0 || workers.len() > 0  {
+        println!("Workers before: {:?}", workers);
+        {
+            if let Some(next_timing) = workers.iter().map(|w| w.1).min() {
+                required_time += next_timing;
+
+
+                for worker in workers.clone().iter() {
+                    workers.retain(|w| w != worker);
+
+                    if worker.1 <= next_timing {
+                        dependencies.remove(worker.0);
+                        result.push_str(worker.0);
+                    } else {
+                        workers.push((worker.0, worker.1 - next_timing));
+                    }
+                }
+            }
+        }
+        println!("Workers after: {:?}", workers);
+
+        let next_keys = {
+            let mut next_keys = dependencies
+                .iter()
+                .filter(|(source, _t)|
+                    !dependencies.values().any(|v| v.contains(source)) && !workers.iter().any(|w| &&w.0 == source)
+                )
+                .map(|e| *e.0)
+                .take(MAX_WORKERS - workers.len())
+                .collect::<Vec<_>>();
+            next_keys.sort();
+
+            next_keys
+        };
+        
+        for key in next_keys {
+            workers.push((key, get_timing(key)));
+        }
+    }
+    println!("Result: {:?}", result);
+    println!("Required time: {:?}", required_time);
+}
+
 fn main() {
     let mut f = File::open("input.txt").expect("File not found");
 
@@ -56,4 +130,5 @@ fn main() {
         .expect("something went wrong reading the file");
 
     task1(&contents);
+    task2(&contents);
 }
